f177094496388d44a6bd08914cc0164ebcc4ae77
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2014-05-03 03:33:54 +0530

    Same Tree

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ LeetCode
 
 Technical Interview Problems: http://leetcode.com
 
-Solved: 7/151
+Solved: 8/151
---
+++ src/SameTree/Solution.java
@@ -0,0 +1,28 @@
+package SameTree;
+
+/**
+ * Created by sitesh on 3/5/14.
+ */
+
+/**
+ * Definition for binary tree
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return p == null && q == null;
+        }
+    }
+}
